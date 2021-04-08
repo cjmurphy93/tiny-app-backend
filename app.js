49b4a2878669b44a934d28b2a6409e3a56b7dcaa
cjmurphy93 <cjmurph93@gmail.com>
2021-04-08 14:34:54 -0400

    Mongo is connected

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoode");
+const mongoose = require("mongoose");
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Mongo is connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("This works!"));
 
