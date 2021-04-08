0e64aae66bf4218eeeafcf17d36197b07e5cadfb
cjmurphy93 <cjmurph93@gmail.com>
2021-04-08 15:09:50 -0400

    Got test request working

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const questions = require("../backend/routes/api/questions");
 
+app.use(express.json());
+
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Mongo is connected"))
